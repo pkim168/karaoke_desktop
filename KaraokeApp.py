e79d56fa4bd2b01d08c7a89c756fe5db848a5464
Paul Kim <pkim168@gmail.com>
2020-02-22 15:13:42 -0500

    Folder structure and App Container

--- KaraokeApp.py
+++ KaraokeApp.py
@@ -1,12 +1,40 @@
 import tkinter as tk
-import MenuPage, ScorePage, PlayerPage
+from tkinter import font  as tkfont
+import page
 
 class KaraokeApp(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
 
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+
+        # Creates a container to hold all frames
         container = tk.Frame(self)
         container.pack(side="top", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-        self.test = tk.Label(self, text="This is the start page", width=20, borderwidth=1, relief="solid")
-        self.test.place(relx=1.0, rely=1.0, x=2, y=2, anchor="nw")
+        # Creates array of frames
+        self.frames = {}
+        for i in (page.MenuPage, page.ScorePage, page.PlayerPage):
+            page_name = i.__name__
+            frame = i(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            # Puts all frames in same location
+            # If frame is smaller than previous, will align to Top left
+            if page_name == 'MenuPage':
+                frame.grid(row=0, column=0, sticky="nw")
+            else:
+                frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("PlayerPage")
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+if __name__ == '__main__':
+    root = KaraokeApp()
+    root.mainloop()
--- MenuPage.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-
-class MenuPage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
